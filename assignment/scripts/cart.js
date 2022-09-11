08ca636d6b6b383baa432489da6aef0325a02f52
Juan Villegas <jvillegas422@gmail.com>
2022-09-11 12:15:51 -0500

    Added empty basket function

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -9,7 +9,6 @@ function addItem( item ) {
     basket.push(item);
     return true;
 }
-
 // Testing add item function
 console.log('Adding Apple: ' + addItem('apple'));
 console.log('Adding Orange: ' + addItem('orange'));
@@ -21,6 +20,14 @@ function listItems( basket ) {
         console.log(basket[i]);
     }
 }
-
 // Testing listItem function
 listItems(basket);
+
+// Empty basket function
+function empty( basket ) {
+    basket.length = 0;
+    console.log(`Basket is empty. ${basket}`);
+}
+// Testing Basket function
+empty(basket);
+console.log(basket);
