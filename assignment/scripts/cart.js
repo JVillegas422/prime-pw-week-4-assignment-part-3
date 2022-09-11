95b200e94d7235a4c7e7db6c9b5c239da85c742b
Juan Villegas <jvillegas422@gmail.com>
2022-09-11 12:07:37 -0500

    Updated addItem & listItems function

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,26 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+
+let basket = [];
+
+// Add item function
+function addItem( item ) {
+    basket.push(item);
+    return true;
+}
+
+// Testing add item function
+console.log('Adding Apple: ' + addItem('apple'));
+console.log('Adding Orange: ' + addItem('orange'));
+console.log('Adding Banana: ' + addItem('banana'));
+
+// List listItem function
+function listItems( basket ) {
+    for (let i = 0; i < basket.length; i++) {
+        console.log(basket[i]);
+    }
+}
+
+// Testing listItem function
+listItems(basket);
